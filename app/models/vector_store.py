aa91bca624a722aae42f0d7358cf494bd8f3c178
miteshupadhyay <upadhyaymitesh91@gmail.com>
2026-09-18 09:24:10 +0530

    Vector Store Added

---
+++ app/models/vector_store.py
@@ -0,0 +1,17 @@
+import chromadb
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+class VectorStore:
+    def __init__(self, vectordb_path):
+        self.embeddings = OpenAIEmbeddings()
+        self.vectore_store = Chroma(
+            persist_directory=vectordb_path,
+            embedding_function=self.embeddings
+        )
+
+    def add_documents(self, documents):
+        self.vectore_store.add_documents(documents)
+
+    def similarity_search(self,query,k =4):
+        return self.vectore_store.similarity_search(query,k=k)
